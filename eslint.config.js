8274893e080e8360714a48244aba0fee25ebe41b
Liliya Kushnikova <iamlilkush4@gmail.com>
2026-04-07 11:33:28 +0300

    Подключить контроль границ для импорта

--- eslint.config.js
+++ eslint.config.js
@@ -3,21 +3,54 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import boundaries from 'eslint-plugin-boundaries'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'node_modules']),
+
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
   {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
+    files: ['src/**/*.{ts,tsx}'],
+    plugins: {
+      boundaries,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
     languageOptions: {
       ecmaVersion: 2020,
+      sourceType: 'module',
       globals: globals.browser,
+    },
+    settings: {
+    'boundaries/elements': [
+      { type: 'app', pattern: 'src/app/**' },
+      { type: 'pages', pattern: 'src/pages/*/**' },
+      { type: 'widgets', pattern: 'src/widgets/*/**' },
+      { type: 'features', pattern: 'src/features/*/**' },
+      { type: 'entities', pattern: 'src/entities/*/**' },
+      { type: 'shared', pattern: 'src/shared/**' },
+    ],
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      ...reactRefresh.configs.vite.rules,
+
+      'boundaries/element-types': [
+        'error',
+        {
+          default: 'disallow',
+          rules: [
+            { from: 'app', allow: ['pages', 'widgets', 'features', 'entities', 'shared'] },
+            { from: 'pages', allow: ['widgets', 'features', 'entities', 'shared'] },
+            { from: 'widgets', allow: ['features', 'entities', 'shared'] },
+            { from: 'features', allow: ['entities', 'shared'] },
+            { from: 'entities', allow: ['shared'] },
+            { from: 'shared', allow: ['shared'] },
+          ],
+        },
+      ],
     },
   },
 ])
