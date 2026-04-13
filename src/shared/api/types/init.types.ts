04a366965f42eec0a74bdb9abe12e13cd4a015c0
Maksim <mkotyukov5@gmail.com>
2026-04-13 22:55:22 +0300

    fix: правки после ревью

--- src/shared/api/types/init.types.ts
+++
@@ -1,9 +0,0 @@
-import type { ISocial, TLanguageCode } from "./common.types";
-
-export type TInitResponse = {
-  site_name: string;
-  seo_description: string;
-  languages: Array<{ code: TLanguageCode; label: string }>;
-  socials: ISocial[];
-  copyright: string;
-};
--- src/shared/api/types/politics.types.ts
+++
@@ -1,3 +0,0 @@
-export type TPoliticsResponse = {
-  text: string;
-};
