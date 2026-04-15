a5f3749a2d735abb657eee8810cdc9fc62b69957
Elena Ishmukhametova <renbiwlen@gmail.com>
2026-04-15 14:49:37 +0300

    Исправить ошибку сборки

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,7 @@
-import { useTranslation } from "react-i18next"
+//import { useTranslation } from "react-i18next"
 
 function App() {
-  const { t } = useTranslation("common");
+  //const { t } = useTranslation("common");
   return <div></div>;
 }
 
