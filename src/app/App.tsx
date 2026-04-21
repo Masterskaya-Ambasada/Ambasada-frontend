5392f13d3b9b09b4382346809fa2b5ad18f80bc6
Liliya Kushnikova <iamlilkush4@gmail.com>
2026-04-21 21:26:02 +0300

    Переместить роутер в папку провайдер

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter } from "react-router-dom";
-import { AppRouter } from "./router/AppRouter";
+import { AppRouter } from "./providers/router";
 //import { useTranslation } from "react-i18next"
 
 function App() {
