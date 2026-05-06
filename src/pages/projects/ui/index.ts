925561837fa59967416051b94d87d9b5e0f81b9c
unknown <lipkinal@ya.ru>
2026-05-06 23:22:16 +0300

    добавить фильтр по типам

--- src/pages/projects/ui/index.ts
+++
@@ -1,9 +0,0 @@
-export { TagsFilter } from "./TagsFilter/TagsFilter";
-export { TypeFilter } from "./TypeFilter/TypeFilter";
-export type { 
-  ProjectType, 
-  ProjectTag, 
-  FiltersResponse,
-  TypeFilterProps,
-  TagsFilterProps 
-} from "./types";
