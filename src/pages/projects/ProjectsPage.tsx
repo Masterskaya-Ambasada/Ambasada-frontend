6eb37f7e36705bf4e28e2cbefd654cafde5962a9
unknown <lipkinal@ya.ru>
2026-07-02 21:37:08 +0300

    убрать конфликт

--- src/pages/projects/ProjectsPage.tsx
+++ src/pages/projects/ProjectsPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { routesPaths } from "@shared/config/routesPaths.ts";
 import { useViewportWidth } from "@shared/lib/useWidthViewPort";
